f7fa24aaf0dc7a67483054a7622d9014b746d17e
nishihamatakashi <nishihamatakashi220@gmail.com>
2026-03-31 16:41:07 +0900

    要素毎にUndoRedoスタックを積むための処理を追加

--- Libs/SlugEditor.Core/Models/UndoRedoModel.cs
+++ Libs/SlugEditor.Core/Models/UndoRedoModel.cs
@@ -1,3 +1,4 @@
+using System.ComponentModel;
 using System.Reflection;
 using System.Runtime.CompilerServices;
 
@@ -7,6 +8,10 @@ namespace SlugEditor.Core.Models;
 
 public class UndoRedoModel : ModelBase
 {
+    private readonly Dictionary<(INotifyPropertyChanged Target, string PropertyName), object?> _nestedPropertyCache = [];
+    private readonly List<(INotifyPropertyChanged Target, PropertyChangedEventHandler Handler)> _nestedSubscriptions = [];
+    private int _nestedReplayDepth;
+
     protected UndoRedoModel(History? history = null)
     {
         History = history ?? new History();
@@ -39,6 +44,102 @@ public class UndoRedoModel : ModelBase
             redo: () => SetTrackedPropertyValue(propertyName, newValue));
 
         return true;
+    }
+
+    protected void TrackNestedProperties(
+        INotifyPropertyChanged nestedObject,
+        string ownerPropertyName,
+        params string[] ignoredPropertyNames)
+    {
+        ArgumentNullException.ThrowIfNull(nestedObject);
+        ArgumentException.ThrowIfNullOrWhiteSpace(ownerPropertyName);
+        var ignored = ignoredPropertyNames.Length is 0
+            ? null
+            : new HashSet<string>(ignoredPropertyNames, StringComparer.Ordinal);
+
+        var properties = nestedObject
+            .GetType()
+            .GetProperties(BindingFlags.Instance | BindingFlags.Public)
+            .Where(p => p.CanRead && p.CanWrite && (ignored is null || !ignored.Contains(p.Name)))
+            .ToArray();
+
+        foreach (var property in properties)
+        {
+            _nestedPropertyCache[(nestedObject, property.Name)] = property.GetValue(nestedObject);
+        }
+
+        PropertyChangedEventHandler handler = (_, e) =>
+        {
+            if (string.IsNullOrWhiteSpace(e.PropertyName))
+            {
+                return;
+            }
+            if (ignored is not null && ignored.Contains(e.PropertyName))
+            {
+                return;
+            }
+
+            var changedProperty = nestedObject.GetType().GetProperty(
+                e.PropertyName,
+                BindingFlags.Instance | BindingFlags.Public);
+            if (changedProperty is null || !changedProperty.CanRead || !changedProperty.CanWrite)
+            {
+                return;
+            }
+
+            var key = (nestedObject, e.PropertyName);
+            if (!_nestedPropertyCache.TryGetValue(key, out var oldValue))
+            {
+                oldValue = changedProperty.GetValue(nestedObject);
+                _nestedPropertyCache[key] = oldValue;
+                return;
+            }
+
+            var newValue = changedProperty.GetValue(nestedObject);
+            if (Equals(oldValue, newValue))
+            {
+                return;
+            }
+
+            _nestedPropertyCache[key] = newValue;
+
+            if (_nestedReplayDepth > 0 || History.IsInUndoing)
+            {
+                OnPropertyChanged(ownerPropertyName);
+                return;
+            }
+
+            History.Push(
+                undo: () =>
+                {
+                    SetNestedPropertyValue(nestedObject, changedProperty, oldValue);
+                    OnPropertyChanged(ownerPropertyName);
+                },
+                redo: () =>
+                {
+                    SetNestedPropertyValue(nestedObject, changedProperty, newValue);
+                    OnPropertyChanged(ownerPropertyName);
+                });
+
+            OnPropertyChanged(ownerPropertyName);
+        };
+
+        nestedObject.PropertyChanged += handler;
+        _nestedSubscriptions.Add((nestedObject, handler));
+    }
+
+    private void SetNestedPropertyValue(INotifyPropertyChanged target, PropertyInfo property, object? value)
+    {
+        _nestedReplayDepth++;
+        try
+        {
+            property.SetValue(target, value);
+            _nestedPropertyCache[(target, property.Name)] = value;
+        }
+        finally
+        {
+            _nestedReplayDepth--;
+        }
     }
 
     private void SetTrackedPropertyValue<T>(string propertyName, T value)
